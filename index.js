59ee9237f1cec5f9d09809a148b8805e515707c8
Badi-Haki Martin <jojackblack@gmail.com>
2022-03-07 15:07:00 -0500

    assignment completed

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+let captializeNextCharacter = false;
+
 const titleCased = () => {
-  return tutorials
+  // map the spaces
+  // console.log(`tutorials include: ${tutorials}`)
+  return tutorials.map(checkLine)
+}
+
+function checkLine(line){
+  console.log(`current line is ${line}`);
+  /*
+  for (let i = 0; i < line.lenth; i++){
+    // if the current character is a space, the next character needs to be capitalized
+    if(line[i] === ` `){
+      console.log(`changing next character: "${line[i++]}"`);
+      line[i++] = line[i++].toUpperCase();
+      console.log(`character changed to: "${line[i++]}"`);
+    }
+  }
+  */
+  const newLine = line.split(' ');
+  return newLine.map(function(word){
+    // console.log(`changing word: ${word.replace(word[0], word[0].toUpperCase())}`);
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(' ')
+ // line.map(checkForSpaces)
 }
